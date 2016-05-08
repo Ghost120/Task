956173486b6abd74c3ec2b27315d85a758b5537c
Ghost120 <ghost120@mail.ru>
2016-05-08 21:22:41 +0300

    добавлен 1

--- CollectionTask/src/collectiontask/CollectionTask.java
+++ CollectionTask/src/collectiontask/CollectionTask.java
@@ -5,15 +5,69 @@
  */
 package collectiontask;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author Kasyanenko Konstantin
  */
 public class CollectionTask {
+ //private List<String> list;
 
-   
-    public static void main(String[] args) {
-        System.out.println("xcv");
+    public static void main(String[] args) throws IOException {
+        task1();
+        task2();
+
     }
-    
+    /**
+     * Возвращает Arraylist из файла
+     */
+    private static List<String> readFileToList(String fileName) {
+        List<String> list = new ArrayList<>();
+        try {
+
+            Path path = Paths.get(fileName);
+            list = Files.readAllLines(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+
+    }
+/**
+ * Записывает List в файл
+ * @param list
+ * @param fileName
+ * @throws IOException 
+ */
+    private static void writeListInFile(List<String> list, String fileName) throws IOException {
+        Path path = Paths.get(fileName);
+        if (Files.notExists(path)) {
+            Files.createFile(path);
+        }
+        Files.write(path, list);
+    }
+
+    private static void task1() throws IOException {
+        System.out.println("1. Ввести строки из файла, записать в список." + "\n"
+                + " Вывести строки в файл в обратном порядке.");
+        String fileName = "src\\file.txt";
+        List<String> list = readFileToList(fileName);
+        System.out.println(list);
+        Collections.reverse(list);
+        writeListInFile(list, fileName);
+        System.out.println(list);
+
+    }
+
+    private static void task2() {
+        
+    }
+
 }
